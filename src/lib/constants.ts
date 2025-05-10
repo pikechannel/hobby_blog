6c8827d4d71136dc79973218f2999c27b06ed670
pikechannel <pikechannel7@gmail.com>
2025-05-10 15:36:20 +0900

    環境変数を読み込むように修正

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const PUBLIC_TITLE = import.meta.env.PUBLIC_TITLE;
+export const PUBLIC_TWITTER_URL = import.meta.env.PUBLIC_TWITTER_URL;
+export const PUBLIC_GITHUB_URL = import.meta.env.PUBLIC_GITHUB_URL;
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		deno(),
-	]
+	],
+	envPrefix: ['PUBLIC_']
 });
