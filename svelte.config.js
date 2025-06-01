058d2cb139022e914014ca733093e2040158e618
pikechannel <pikechannel7@gmail.com>
2025-06-01 16:12:55 +0900

    vercelの設定を修正

--- svelte.config.js
+++ svelte.config.js
@@ -13,6 +13,9 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			runtime: 'nodejs18.x',
+			regions: ['hnd1'],
+			split: false,
+			fallback: 'index.html'
 		}),
 		files: {
 			assets: 'static',
