4f0488502fc4be2092cd074edd860c1583fffde4
pikechannel <pikechannel7@gmail.com>
2025-06-08 06:04:41 +0900

    vercelテスト

--- src/routes/article/[id]/+page.server.ts
+++ src/routes/article/[id]/+page.server.ts
@@ -19,14 +19,14 @@ export const load = (async ({ params }) => {
             throw error(404, 'Article ID is required');
         }
 
-        const filePath = path.join(process.cwd(), 'contents', 'articles', `${id}.svx`);
+        const filePath = path.join(process.cwd(), 'static', 'contents', 'articles', `${id}.svx`);
         const slug = path.basename(filePath, '.svx');
 
         // 開発環境と本番環境で異なるパスを使用
         const isDev = process.env.NODE_ENV === 'development';
         const importPath = isDev 
             ? `../../../../contents/articles/${id}.svx`
-            : `../../../../contents/articles/${id}.svx.js`;
+            : `/contents/articles/${id}.svx`;
 
         const module = await import(importPath /* @vite-ignore */);
         const metadata = module.metadata;
