026ed623e9330ec7f8f3ee1185fdfd77e5d1d68e
pikechannel <pikechannel7@gmail.com>
2025-06-08 04:58:39 +0900

    vercel用の修正

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,20 +4,17 @@ import type { ServerLoad } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 import { getArticlePath } from '$lib/store';
 
-interface Metadata {
-  title: string;
-  date: string;
-  category: string;
-  description: string;
-  thumbnail: string;
-}
-
-interface ArticleInfo {
-  slug: string;
-  metadata: Metadata;
+interface Article {
+    slug: string;
+    metadata: {
+        date: string;
+        title: string;
+        description: string;
+        tags: string[];
+    };
 }
 
 export const load: ServerLoad = async () => {
-  const articles = get(getArticlePath);
-  return { articles };
+    const articles = get(getArticlePath);
+    return { articles };
 };
